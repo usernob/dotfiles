c4426ccd19f430593e32a9d5c07d8a2067226c8e
usernob <shamsulmaarif2016@gmail.com>
2024-03-25 11:13:31 +0700

    fix: starship module

--- nvim/.config/nvim/lua/configs/conform.lua
+++ nvim/.config/nvim/lua/configs/conform.lua
@@ -1,10 +1,24 @@
 local options = {
-  lsp_fallback = true,
+	lsp_fallback = true,
 
-  formatters_by_ft = {
-    lua = { "stylua" },
-    rust = { "rustfmt" },
-  },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		rust = { "rustfmt" },
+    typescript = { "prettier" },
+    typescriptreact = { "prettier" },
+    javascript = { "prettier" },
+    javascriptreact = { "prettier" },
+    css = { "prettier" },
+    html = { "prettier" },
+    markdown = { "prettier" },
+    json = { "prettier" },
+    jsonc = { "prettier" },
+    yaml = { "prettier" },
+    graphql = { "prettier" },
+    scss = { "prettier" },
+    less = { "prettier" },
+    vue = { "prettier" },
+	},
 }
 
 require("conform").setup(options)
