4b209679d51326d6db5d7d78911bddbf4f084ca3
usernob <shamsulmaarif2016@gmail.com>
2024-06-03 12:07:58 +0700

    drop sway -> migrate to hyprland

--- sway/.config/sway/idle/inhibitors/pulse.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-pactl list | grep RUNNING && exit 1 || exit 0
--- sway/.config/sway/idle/suspend.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# inhibited?
-~/.config/sway/idle/inhibitors/pulse.sh || exit 1
-
-sudo systemctl suspend
