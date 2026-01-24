e33295ad22418773c88ef289c6a41248109ef7dd
usernob <contact.ramzipandu@gmail.com>
2026-01-24 09:56:37 +0700

    chore(nvim): update lazydev

--- nvim/.config/nvim/lua/plugins/lazydev.lua
+++ nvim/.config/nvim/lua/plugins/lazydev.lua
@@ -1,12 +1,10 @@
 return {
-    {
-        "folke/lazydev.nvim",
-        ft = "lua",
-        opts = {
-            library = {
-                { path = "luvit-meta/library", words = { "vim%.uv" } },
-            },
+    "folke/lazydev.nvim",
+    ft = "lua",
+    opts = {
+        library = {
+            -- Load luvit types when the `vim.uv` word is found
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
         },
     },
-    { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
 }
