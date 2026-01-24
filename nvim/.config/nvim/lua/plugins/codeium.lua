9213a8c8ed474a72094e199a1dc0e8908a0d4327
usernob <contact.ramzipandu@gmail.com>
2026-01-24 08:14:10 +0700

    chore(nvim): update codeium

--- nvim/.config/nvim/lua/plugins/codeium.lua
+++ nvim/.config/nvim/lua/plugins/codeium.lua
@@ -1,19 +1,47 @@
+--- @module 'lazy.types'
+--- @type LazySpec
 return {
-    "Exafunction/codeium.vim",
+    "Exafunction/windsurf.nvim",
     event = "InsertEnter",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
     config = function()
-        -- Change '<C-g>' here to any keycode you like.
-        vim.keymap.set("i", "<C-g>", function()
-            return vim.fn["codeium#Accept"]()
-        end, { expr = true })
-        vim.keymap.set("i", "<C-d>", function()
-            return vim.fn["codeium#CycleCompletions"](1)
-        end, { expr = true })
-        vim.keymap.set("i", "<C-f>", function()
-            return vim.fn["codeium#CycleCompletions"](-1)
-        end, { expr = true })
-        vim.keymap.set("i", "<M-w>", function()
-            return vim.fn["codeium#Clear"]()
-        end, { expr = true })
+        local opts = {
+            enable_cmp_source = false,
+            virtual_text = {
+                enabled = true,
+                key_bindings = {
+                    -- Accept the current completion.
+                    accept = "<C-g>",
+                    -- Accept the next word.
+                    accept_word = false,
+                    -- Accept the next line.
+                    accept_line = false,
+                    -- Clear the virtual text.
+                    clear = false,
+                    -- Cycle to the next completion.
+                    next = "<M-]>",
+                    -- Cycle to the previous completion.
+                    prev = "<M-[>",
+                },
+            },
+            workspace_root = {
+                use_lsp = true,
+                find_root = nil,
+                paths = {
+                    ".bzr",
+                    ".git",
+                    ".hg",
+                    ".svn",
+                    "_FOSSIL_",
+                    "package.json",
+                    "CMakeLists.txt",
+                },
+            },
+        }
+        require("codeium").setup(opts)
+        -- hack to make virtual_text match the colorscheme
+        vim.api.nvim_set_hl(0, "CodeiumSuggestion", { link = "Comment" })
     end,
 }
